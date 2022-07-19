f9987ddb4aa4ea3f32f11aff37bd460cec763fd5
dohoangdinhtien <tiendo152@gmail.com>
2022-07-19 14:30:48 +0700

    change app one

--- apps/app-one/src/main.ts
+++ apps/app-one/src/main.ts
@@ -8,7 +8,7 @@ import * as express from 'express';
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to app-one!' });
+  res.send({ message: 'Welcome to app-one - Changed!' });
 });
 
 const port = process.env.port || 3333;

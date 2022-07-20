4511cc9b57d6ddc7f8b4bd65e55d38734716ed62
dohoangdinhtien <tiendo152@gmail.com>
2022-07-20 09:18:44 +0700

    change docker file app-two

--- apps/app-two/src/main.ts
+++ apps/app-two/src/main.ts
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+ import * as express from 'express';
+
+ const app = express();
+
+ app.get('/api', (req, res) => {
+   res.send({ message: 'Welcome to app-two - Changed 1!' });
+ });
+
+ const port = process.env.port || 3000;
+ const server = app.listen(port, () => {
+   console.log(`Listening at http://localhost:${port}/api`);
+ });
+ server.on('error', console.error);
